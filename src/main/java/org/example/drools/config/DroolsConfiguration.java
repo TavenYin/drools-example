5b75a8048b0e4ea4b9f549a3fd45f21580fb4fe1
TavenYin <yintianwen7@qq.com>
2022-06-08 15:24:39 +0800

    创建 KieContainer 后，对 KieBase 进行检查

--- src/main/java/org/example/drools/config/DroolsConfiguration.java
+++ src/main/java/org/example/drools/config/DroolsConfiguration.java
@@ -1,13 +1,19 @@
 package org.example.drools.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.kie.api.KieServices;
+import org.kie.api.builder.Message;
+import org.kie.api.builder.Results;
 import org.kie.api.runtime.KieContainer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Collection;
+
 /**
  * @author tianwen.yin
  */
+@Slf4j
 @Configuration
 public class DroolsConfiguration {
     public DroolsConfiguration() {
@@ -16,6 +22,31 @@ public class DroolsConfiguration {
     @Bean
     public KieContainer classpathKContainer() {
         KieServices kieServices = KieServices.Factory.get();
-        return kieServices.newKieClasspathContainer();
+        KieContainer kc = kieServices.newKieClasspathContainer();
+        Collection<String> kBaseNames = kc.getKieBaseNames();
+
+        for (String kBaseName : kBaseNames) {
+            Results verify;
+            try {
+                verify = kc.verify(kBaseName);
+            } catch (Exception e) {
+                throw new RuntimeException("verify exception");
+            }
+
+            if (verify.hasMessages(Message.Level.INFO)) {
+                log.info(verify.getMessages(Message.Level.INFO).toString());
+            }
+
+            if (verify.hasMessages(Message.Level.WARNING)) {
+                log.warn(verify.getMessages(Message.Level.WARNING).toString());
+            }
+
+            if (verify.hasMessages(Message.Level.ERROR)) {
+                log.error(verify.getMessages(Message.Level.ERROR).toString());
+                throw new RuntimeException(String.format("Exception occurred during verify kBase %s", kBaseName));
+            }
+        }
+
+        return kc;
     }
 }
